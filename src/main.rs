98f2d7801a67ab28cac70269748a3db802542c89
Nihal Developer <nihaltmdev@gmail.com>
2023-08-17 22:47:48 +0530

    update cargo.toml and main.rs with some more testing related code

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,20 @@ fn test_runner(tests: &[&dyn Fn()]) {
     exit_qemu(QemuExitCode::Success);
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T 
+where T:Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[test_case]
 fn trivial_assertion() {
     serial_print!("trivial assertion...");
