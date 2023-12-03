a81a60e43447fff909ed2b17c6250afe83b03c67
nihaltm <nihaltm@nihaltm.nihaltm.com>
2023-12-03 13:03:46 +0530

    vga_buffer code!

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
   loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop{}
+    println!("Hello Peeps!");
+
+    loop {}
 }
